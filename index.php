847cc3f0654125e521c160ea3d4d5748c96bddf7
JuanSeptian <juanseptian06@gmail.com>
2023-09-11 19:01:06 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello, World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium numquam architecto neque, iste quidem voluptatem dignissimos unde commodi, labore ipsa doloremque ipsum dolores. Quisquam voluptas, minus doloremque nihil expedita illo.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
